15e94c6fc6356dce5e56df8d8ac48af9d9182474
Nick Jones <nick.jones@upguard.com>
2023-12-18 12:30:34 +1100

    Matrix refinements.

--- internal/state/matrix.go
+++ internal/state/matrix.go
@@ -1,5 +1,6 @@
 package state
 
+// identity returns the identity matrix.
 func identity() *matrix {
 	return &matrix{
 		{1, 0, 0},
@@ -8,11 +9,13 @@ func identity() *matrix {
 	}
 }
 
+// matrix represents 3x3 matrices for scale, rotation and translation for text and graphics state.
+// The final column of these matrixes is always 0,0,1.
 type matrix [3][3]float64
 
 func (m *matrix) Mul(n *matrix) *matrix {
 
-	if m.sparse() && n.sparse() {
+	if !m.rotation() && !n.rotation() {
 		return &matrix{
 			{m[0][0] * n[0][0], 0, 0},
 			{0, m[1][1] * n[1][1], 0},
@@ -20,20 +23,13 @@ func (m *matrix) Mul(n *matrix) *matrix {
 		}
 	}
 
-	var mn matrix
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			for k := 0; k < 3; k++ {
-				mn[i][j] += m[i][k] * n[k][j]
-			}
-		}
+	return &matrix{
+		{m[0][0]*n[0][0] + m[0][1]*n[1][0], m[0][0]*n[0][1] + m[0][1]*n[1][1], 0},
+		{m[1][0]*n[0][0] + m[1][1]*n[1][0], m[1][0]*n[0][1] + m[1][1]*n[1][1], 0},
+		{m[2][0]*n[0][0] + m[2][1]*n[1][0] + n[2][0], m[2][0]*n[0][1] + m[2][1]*n[1][1] + n[2][1], 1},
 	}
-
-	return &mn
 }
 
-func (m *matrix) sparse() bool {
-	return m[0][1] == 0 && m[0][2] == 0 &&
-		m[1][0] == 0 && m[1][2] == 0 &&
-		m[2][2] == 1
+func (m *matrix) rotation() bool {
+	return m[0][1] != 0 || m[1][0] != 0
 }
